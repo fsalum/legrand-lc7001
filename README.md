95f52d3e40ec14b1e47334ea20efc5e92cf70670
Felipe Salum <fsalum@apple.com>
2020-11-10 22:42:27 -0800

    1st version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # legrand-lc7001
 Proof of Concept to authenticate to Legrand Hub LC7001
+
+Latest LC7001 firmware update added authentication and broke a few integrations with the Hub. After talking to Legrand Technical team and explaining the situation they agreed and sent me some documents explaining how the authentication is handled.
+
+I implemented this POC in Python 3 to test it out and so far as today it works perfectly.
+
+```bash
+$ ./legrand.py
+Socket Created
+Socket Connected to LCM1.local on ip 192.168.1.112
+Challenge: 51C8D6C53A0DF927FF01F03E46575E26
+Encrypted Response C4CF376EE064BDF2529FDAAD361B7DB6
+[OK]
+
+{"ID":0,"Service":"ping","CurrentTime":1605076627,"PingSeq":1,"Status":"Success"}
+```
---
+++ legrand.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+from Crypto.Cipher import AES
+import binascii
+import hashlib
+import socket
+import sys
+import time
+
+
+HOST = 'LCM1.local'
+PORT = 2112
+LC7001_PASSWORD = b'12345678'
+
+
+def socket_connection():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print('Failed to create socket')
+        sys.exit()
+
+    print('Socket Created')
+
+    try:
+        remote_ip = socket.gethostbyname(HOST)
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    s.connect((remote_ip , PORT))
+    print('Socket Connected to ' + HOST + ' on ip ' + remote_ip)
+
+    return s
+
+
+def recv_timeout(the_socket,timeout=5):
+    #make socket non blocking
+    the_socket.setblocking(0)
+
+    #total data partwise in an array
+    total_data=[];
+    data='';
+
+    #beginning time
+    begin=time.time()
+    while 1:
+        #if you got some data, then break after timeout
+        if total_data and time.time()-begin > timeout:
+            break
+
+        #if you got no data at all, wait a little longer, twice the timeout
+        elif time.time()-begin > timeout*2:
+            break
+
+        #recv something
+        try:
+            data = str(the_socket.recv(8192).decode('ascii'))
+            if data:
+                total_data.append(data)
+                #change the beginning time for measurement
+                begin=time.time()
+            else:
+                #sleep for sometime to indicate a gap
+                time.sleep(0.1)
+        except:
+            pass
+
+    #join all parts to make final string
+    return ''.join(total_data)
+
+
+def encrypt_key(challenge):
+    private_key = binascii.unhexlify(hashlib.md5(LC7001_PASSWORD).hexdigest())
+    encryptor = AES.new(private_key, AES.MODE_ECB)
+    lc7001_hello = binascii.unhexlify(challenge)
+    ciphertext = encryptor.encrypt(lc7001_hello)
+    challenge_answer = binascii.hexlify(ciphertext)
+    print('Encrypted Response', challenge_answer.decode('ascii').upper())
+
+    return challenge_answer
+
+
+if __name__ == "__main__":
+    s = socket_connection()
+
+    total_data = recv_timeout(s).split( )
+    total_data = [x.replace('\x00', '') for x in total_data]
+    challenge = total_data[2]
+    print("Challenge:", challenge)
+
+    challenge_answer = encrypt_key(challenge)
+
+    #Answer the challenge to the server
+    try:
+        s.sendall(challenge_answer)
+        print(recv_timeout(s))
+    except socket.error:
+        print('Send failed')
+        sys.exit(1)
+
+    #Close the socket
+    s.close()
